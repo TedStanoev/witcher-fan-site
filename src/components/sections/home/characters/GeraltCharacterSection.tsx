08dc3c663566c0246ede1b62c29b9a8118c2d605
Teodor Stanoev <teodor.stanoev@pateplay.com>
2026-02-05 10:36:23 +0200

    Improve responsiveness for GeraltSection

--- src/components/sections/home/characters/GeraltCharacterSection.tsx
+++ src/components/sections/home/characters/GeraltCharacterSection.tsx
@@ -1,21 +1,43 @@
 'use client';
 
 import { useGSAP } from '@gsap/react';
-import gsap from '@/libs/gsap';
+import gsap, { SplitText } from '@/libs/gsap';
 import Image from 'next/image';
 
 import GeraltSrc from '../../../../../public/images/geralt.png';
 import GeraltBGSrc from '../../../../../public/images/geralt-background.jpg';
+import { breakpoints } from '@/hooks/useResponsive';
 
 import './GeraltCharacterSection.scss';
+import { useRef } from 'react';
 
 type TProps = { id?: string };
 
 export default function GeraltCharacterSection({ id }: TProps) {
-  useGSAP(() => {}, []);
+  const sectionRef = useRef<HTMLDivElement>(null);
+
+  useGSAP(() => {
+    // gsap.matchMedia().add(`(max-width: ${breakpoints.laptop}px)`, () => {
+    //   const textSplit = new SplitText('.Geralt-Character-Section--Quote', {
+    //     type: 'lines',
+    //   });
+
+    //   gsap.from(textSplit.lines, {
+    //     yPercent: 15,
+    //     xPercent: -15,
+    //     opacity: 0,
+    //     ease: 'power1.inOut',
+    //     stagger: 0.3,
+    //     scrollTrigger: {
+    //       trigger: sectionRef.current,
+    //       start: 'top center',
+    //     },
+    //   });
+    // });
+  }, []);
 
   return (
-    <section id={id} className="Geralt-Character-Section">
+    <section ref={sectionRef} id={id} className="Geralt-Character-Section">
       <Image
         src={GeraltBGSrc}
         alt="Geralt Background"
